5c3c5b3563ad11a9b0f11c858268b3f6d1f54d8d
azharmd-dev <azharmd0794@gmail.com>
2025-10-28 15:45:31 +0530

    security group

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,7 +1,18 @@
-module "vote_service_sg" {
+# Using open source module
+/* module "vote_service_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "${local.common_name_suffix}-catalogue"
   description = "Security group for catalogue with custom ports open within VPC"
-  vpc_id      = "vpc-12345678"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+} */
+
+module "sg" {
+  count = length(var.sg_names)
+  source = "git::https://github.com/azharmd-dev/tf-aws-sg.git?ref=main"
+  project_name = var.project_name
+  environment =  var.environment
+  sg_name = var.sg_names[count.index]
+  sg_description = "created for ${var.sg_names[count.index]}"
+  vpc_id = local.vpc_id
 }
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -5,3 +5,20 @@ variable "project_name" {
 variable "environment" {
   default  =   "dev"
 }
+
+variable "sg_names" {
+  default = [
+    #database
+    "mongodb", "redis", "rabbitmq", "mysql",
+    #backend
+    "catalogue", "user", "cart", "shipping", "payment",
+    #frontend
+    "frontend",
+    #bastion
+    "bastion",
+    # frontend load balancer
+    "frontend_alb",
+    # Backend ALB
+    "backend_alb"
+  ]
+}
