ce3d35c9f9fbe53934390aa5f6fbde2db15204a0
azharmd-dev <azharmd0794@gmail.com>
2025-11-03 20:46:08 +0530

    updated

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -3,6 +3,13 @@ variable "project_name" {
 }
 
 variable "environment" {
-  default = "dev"
-  
+  default = "dev" 
+}
+
+variable "zone_id" {
+    default = "Z0553382WPE1E3VPCYS6"
+}
+
+variable "domain_name" {
+    default = "azharprojects.site"
 }
