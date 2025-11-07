6cec8a6dd62d462bba9a2758a9848c4e69e459b5
azharmd-dev <azharmd0794@gmail.com>
2025-11-07 20:56:29 +0530

    catalogue application setup

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -6,6 +6,7 @@ locals {
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_id.value)
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   vpc_id =  data.aws_ssm_parameter.vpc_id.value
+  backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   common_tags = {
         Project = var.project_name
         Environment = var.environment
