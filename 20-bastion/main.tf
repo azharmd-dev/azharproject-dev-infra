fdef2d224ffb4b3171d7d2fc7d3ef39265ef4814
azharmd-dev <azharmd0794@gmail.com>
2025-11-07 09:59:04 +0530

    updated

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -1,9 +1,15 @@
 resource "aws_instance" "bastion" {
-      ami = local.ami_id
-      vpc_security_group_ids = [local.bastion_sg_id]
-      instance_type = local.instance_type
-      subnet_id = local.public_subnet_id
-      associate_public_ip_address = true
+    ami = local.ami_id
+    vpc_security_group_ids = [local.bastion_sg_id]
+    instance_type = local.instance_type
+    subnet_id = local.public_subnet_id
+    associate_public_ip_address = true
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
+    # need more for terraform
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3" # or "gp2", depending on your preference
+    }
       user_data = file("bastion.sh")
       tags = merge(
         local.common_tags,
@@ -13,3 +19,10 @@ resource "aws_instance" "bastion" {
       )
       
     }
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
+}
+
+
