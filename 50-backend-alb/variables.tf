545676e985f113bdeae2bb7a542f150d6f501b34
azharmd-dev <azharmd0794@gmail.com>
2025-11-07 20:55:44 +0530

    backend application load balancer

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0553382WPE1E3VPCYS6"
+}
+
+variable "domain_name" {
+    default = "azharprojects.site"
+}
