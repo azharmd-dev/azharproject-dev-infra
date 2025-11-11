9c41b2b3ccf2d1607b19c895e8c2a927b79a68b9
azharmd-dev <azharmd0794@gmail.com>
2025-11-11 22:54:52 +0530

    aws cerificate manager to generate and validate ssl certicates

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "domain_name" {
+    default = "azharprojects.site"
+}
+
+variable "project_name" {
+  default = "azharproject"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0553382WPE1E3VPCYS6"
+}
