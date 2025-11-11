1a1bd5d8cd66cfdca469bb99494c3606076feb81
azharmd-dev <azharmd0794@gmail.com>
2025-11-11 22:50:32 +0530

    creating security groups for all the resources

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "azhar-terraform-dev"
-    key    = "roboshop-dev-infra-10-sg"
+    key    = "azharprojects-dev-infra-sg"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
