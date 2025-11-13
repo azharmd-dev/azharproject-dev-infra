5d2ad14d3d88c5d9c04ccc7a9ea3293791b13fe2
azharmd-dev <azharmd0794@gmail.com>
2025-11-13 23:21:18 +0530

    setting up all instances using modules

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,12 @@
-module "this" {
+/* module "this" {
     source = "../../tf-aws-components"
     component = var.component
     rule_priority = var.rule_priority
+} */
+
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/azharmd-dev/tf-aws-components.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
